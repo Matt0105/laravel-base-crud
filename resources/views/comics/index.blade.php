075b16d644460cc228dda8b25dc4c18c4e00e5f4
Matt0105 <matteo0105@gmail.com>
2022-02-23 15:52:31 +0100

    added index page with pagination

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,5 +5,39 @@
 @endsection
 
 @section('content')
-    
+<main>
+    <div class="container">
+        <table class="table table-primary">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Brand</th>
+                <th scope="col">Editor</th>
+                <th scope="col">Price</th>
+                <th scope="col">Thumb</th>
+                <th scope="col">Info</th>
+              </tr>
+            </thead>
+
+            @foreach ($comics as $comic)
+                
+                <tbody>
+                    <tr>
+                        <th scope="row">{{$comic->id}}</th>
+                        <td>{{$comic->name}}</td>
+                        <td>{{$comic->brand}}</td>
+                        <td>{{$comic->editor}}</td>
+                        <td>{{$comic->price}}&euro;</td>
+                        <td style="width: 30px"><img class="img-fluid" src="{{$comic->thumb}}" alt="{{$comic->name}}"></td>
+                        <td><a href="{{route("comics.show", $comic)}}"><button class="btn btn-primary">More Info</button></a></td>
+                    </tr>
+                </tbody>
+
+            @endforeach
+          </table>
+          <div style="width: 100%; display: flex; justify-content: center">{{$comics->links()}}</div>
+    </div>
+</main>
+
 @endsection
